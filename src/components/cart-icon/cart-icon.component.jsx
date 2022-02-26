25f1a2397b94388e212c7a22c4d561d6dcc42bb9
Aaron-Guo <gjp77440169@gmail.com>
2022-02-27 00:08:30 +1100

    Change into styled components

--- src/components/cart-icon/cart-icon.component.jsx
+++ src/components/cart-icon/cart-icon.component.jsx
@@ -1,21 +1,28 @@
-import React from "react";
-import { connect } from "react-redux";
-import { toggleCartHidden } from "../../redux/cart/cart-actions";
-import "./cart-icon.styles.scss";
-import { ReactComponent as ShoppingIcon } from "../../assets/shopping-bag.svg";
-import { selectCartItemsCount } from "../../redux/cart/cart.selectors";
-import { createStructuredSelector } from "reselect";
-const CartIcon = ({ toggleCartHidden, itemCount }) => (
-  <div className="cart-icon" onClick={toggleCartHidden}>
-    <ShoppingIcon className="shopping-icon" />
-    <span className="item-count">{itemCount}</span>
-  </div>
+import React from 'react';
+import {connect} from 'react-redux';
+import {createStructuredSelector} from 'reselect';
+
+import {toggleCartHidden} from '../../redux/cart/cart.actions';
+import {selectCartItemsCount} from '../../redux/cart/cart.selectors';
+
+import {CartContainer, ItemCountContainer, ShoppingIcon} from './cart-icon.styles';
+
+const CartIcon = ({toggleCartHidden, itemCount}) => (
+    <CartContainer onClick={toggleCartHidden}>
+        <ShoppingIcon/>
+        <ItemCountContainer>{itemCount}</ItemCountContainer>
+    </CartContainer>
 );
-const mapDispatchToProps = (dispatch) => ({
-  toggleCartHidden: () => dispatch(toggleCartHidden()),
-});
-const mapStateToProps = createStructuredSelector({
-  itemCount: selectCartItemsCount,
+
+const mapDispatchToProps = dispatch => ({
+    toggleCartHidden: () => dispatch(toggleCartHidden())
 });
 
-export default connect(mapStateToProps, mapDispatchToProps)(CartIcon);
+const mapStateToProps = createStructuredSelector({
+    itemCount: selectCartItemsCount
+});
+
+export default connect(
+    mapStateToProps,
+    mapDispatchToProps
+)(CartIcon);
--- src/components/stripe-button/stripe-button.component.jsx
+++ src/components/stripe-button/stripe-button.component.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import StripeCheckout from "react-stripe-checkout";
 
-const StripeCheckoutButton = ({ price }) => {
+const StripeCheckoutButton = ({price}) => {
     const priceForStripe = price * 100;
     const publishableKey = 'pk_test_51KXMyHBIPky85AWTeoMyMhlJRKCRE2u4WvM23qTcuTJeukYIvax5aq8r0ugwMFPNm5ayIpo1xGy3EVkDFe7dduke00v0dYPiNH';
     const onToken = token => {
