f08a7430393b53c3b49b6674053a1ac761c9d5ba
Aaron-Guo <gjp77440169@gmail.com>
2022-02-26 21:23:18 +1100

    Updating stripe integration and updating our favicon and tile

---
+++ src/components/stripe-button/stripe-button.component.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+import StripeCheckout from "react-stripe-checkout";
+
+const StripeCheckoutButton = ({ price }) => {
+    const priceForStripe = price * 100;
+    const publishableKey = 'pk_test_51KXMyHBIPky85AWTeoMyMhlJRKCRE2u4WvM23qTcuTJeukYIvax5aq8r0ugwMFPNm5ayIpo1xGy3EVkDFe7dduke00v0dYPiNH';
+    const onToken = token => {
+        console.log(token);
+        alert('Payment Successful')
+    }
+    return (
+        <StripeCheckout
+            token={onToken}
+            stripeKey={publishableKey}
+            label='Pay Now'
+            name='CRWN Clothing Ltd.'
+            billingAddress
+            shippingAddress
+            image='https://svgshare.com/i/CUz.svg'
+            description={`Your total is $${price}`}
+            amount={priceForStripe}
+            panelLabel='Pay Now'
+        />
+    )
+
+};
+export default StripeCheckoutButton
--- src/pages/checkout/checkout.component.jsx
+++ src/pages/checkout/checkout.component.jsx
@@ -6,6 +6,7 @@ import {
   selectCartTotal,
 } from "../../redux/cart/cart.selectors";
 import CheckoutItem from "../../components/checkout-item/checkout-item.component";
+import StripeCheckoutButton from "../../components/stripe-button/stripe-button.component";
 import "./checkout.styles.scss";
 const CheckoutPage = ({ cartItems, total }) => (
   <div className="checkout-page">
@@ -29,10 +30,15 @@ const CheckoutPage = ({ cartItems, total }) => (
     {cartItems.map((cartItem) => (
       <CheckoutItem key={cartItem.id} cartItem={cartItem} />
     ))}
-    {console.log(cartItems)}
     <div className="total">
       <span>TOTAL: ${total}</span>
     </div>
+    <div className='test-warning'>
+      *Please use the following test credit card for payments*
+        <br/>
+        4242 4242 4242 4242 - Exp: Any future date - CVV: Any 3 digits
+    </div>
+    <StripeCheckoutButton price={total} />
   </div>
 );
 
